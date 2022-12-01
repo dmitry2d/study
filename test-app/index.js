5e11153724d742d5f5a8122afe8579f891ea1f2a
dmitry2d <volcharo@gmail.com>
2022-12-01 16:25:43 +0300

    Blog

--- test-app/index.js
+++ test-app/index.js
@@ -4,6 +4,19 @@ const PORT = 5000;
 
 const app = express ();
 
+app.use (express.json());
+
+
+// app.get ('/', (req, res) => {
+//     console.log (req.query);
+//     res.status(200).json('Сервер работает');
+// });
+
+app.post ('/', (req, res) => {
+    console.log (req.body);
+    res.status(200).json('Сервер работает: ' + req.body.test);
+});
+
 app.listen (PORT, () => {
     console.log ('SERVER STARTED AT PORT ' + PORT);
 });
