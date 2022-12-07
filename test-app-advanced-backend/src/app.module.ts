7e90ce663ca3a49399c98ea1a85ea5b87d50107a
dmitry2d <volcharo@gmail.com>
2022-12-07 16:23:35 +0300

    coding

--- test-app-advanced-backend/src/app.module.ts
+++ test-app-advanced-backend/src/app.module.ts
@@ -1,10 +1,8 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { appController } from "./app.controller";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    controllers: [appController]
 })
+
 export class AppModule {}
