7520202a44dda7eb0433b69aaea8b9759ec378e6
dmitry2d <volcharo@gmail.com>
2022-12-08 11:48:37 +0300

    Code / Blog

--- test-app-advanced-backend/src/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from "@nestjs/common";
-
-@Controller ('/api')
-export class appController {
-    @Get ('/users')
-    getUsers () {
-        [{id: 1, name: 'dmitry2d'}]
-    }
-}
