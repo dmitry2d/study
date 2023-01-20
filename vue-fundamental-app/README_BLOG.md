82b913f588c4440c1fb0f778e9ca15c69bd3e127
dmitry2d <volcharo@gmail.com>
2023-01-20 15:29:13 +0300

    coding

---
+++ vue-fundamental-app/README_BLOG.md
@@ -0,0 +1,22 @@
+# Application "vue-fundamental-app"
+
+## About
+
+## App Name
+
+
+## App Description
+
+## App Functions
+
+## Technical details
+
+* Platform: Ubuntu 18.4
+* Engine: NodeJS
+
+## Blog
+
+#### 2023 Yan 20
+
+* Install Vue CLI
+* Create basic project
--- vue-fundamental-app/src/App.vue
+++ vue-fundamental-app/src/App.vue
@@ -1,11 +1,29 @@
 
 
 <template>
-    
+    <div>Likes: {{ likes }}</div>
+    <div>Dislikes: {{ dislikes }}</div>
+    <button v-on:click="addLike">Like</button>
+    <button v-on:click="addDislike">Dislike</button>
 </template>
 
 <script>
-
+export default {
+    data() {
+        return {
+            likes: 0,
+            dislikes: 0
+        }
+    },
+    methods: {
+        addLike() {
+            this.likes ++;
+        },
+        addDislike() {
+            this.dislikes ++;
+        }
+    }
+}
 </script>
 
 <style>
